6ba6e9e05a54fa9797f51edacc847b8ffde09a97
Gustavo Baldavia <112123706+Gussball@users.noreply.github.com>
2023-06-30 09:47:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(function time() {
+function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
-
     if (min < 10) min = '0' + min;
-
     if (sec < 10) sec = '0' + sec;
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
+}
 
-})
+// Chamar a função time inicialmente para definir o relógio
+time();
 
+// Atualizar o relógio a cada segundo
+const relogio = setInterval(time, 1000);
